14f6c24d75a563db2e8a8270a85ea50495a7d467
AakashR11111001111 <aakash04721202019@msi-ggsip.org>
2025-04-18 11:33:17 +0530

    updated

--- src/Utils/ReactJSProjects.js
+++ src/Utils/ReactJSProjects.js
@@ -5,5 +5,12 @@ export const ReactJSProjects = [
         thumbnail: "/Thumbnails/Codepen.png",
         liveLink: "https://code-pen-clone-bice.vercel.app/",
         gitLink: "https://github.com/AakashR11111001111/CodePen-Clone",
+    },
+    {
+        title: "Trello Clone",
+        desc: "A Kanban-style task management app that allows users to create, update, and drag & drop tasks between columns. Built with React and localStorage for persistent state.",
+        thumbnail: "/Thumbnails/Trello.png",
+        liveLink: "https://trello-like-app-clone.vercel.app/",
+        gitLink: "https://github.com/AakashR11111001111/Trello-like-App-Clone",
     }
 ];
